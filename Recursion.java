00c1518de5a000a1e200b28883a2b76ab9d141b1
Nainakumari7 <118041285+Nainakumari7@users.noreply.github.com>
2024-09-16 09:25:22 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -119,3 +119,141 @@ public class Recursion{
         System.out.println(firstOccurence(arr,5,0));
     }
 }
+public class Recursion{
+    public static int lastOccurence(int arr[],int key,int i){
+        if(i ==arr.length){
+            return -1;
+        }
+        int isFound = lastOccurence(arr, key, i+1);
+        if(isFound != -1){
+            return isFound;
+        }
+
+        if(arr[i]==key){
+            return i;
+        }
+        return isFound;
+
+    }
+            public static void main(String[] args) {
+        int arr[]={8,3,6,9,5,10,2,5,8,3};
+        int key = 5;
+        System.out.println(lastOccurence(arr,5,0));
+    }
+}
+
+public class Recursion{
+    public static int power(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        return  x*power(x,n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(power(2,10));
+    }
+    
+}
+
+
+public class Recursion{
+    public static int optimisedPower(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        int half = optimisedPower(x,n/2)*optimisedPower(x,n/2);
+
+        if(n %2 != 0){
+            half = x*half;
+        }
+        return half;
+    
+    }
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 10;
+        System.out.println(optimisedPower(2,10));
+    }
+    
+}
+
+public class Recursion{
+    public static int till(int n){
+        if(n==0 || n==1){
+            return 1;
+
+        }
+        int fnm1 = till(n-1);
+
+        int fnm2 = till(n-2);
+        return fnm1+fnm2;
+    }
+    public static void main(String[] args) {
+        System.out.println(till(3));
+        
+    }
+}
+
+public class Recursion{
+    public static void removeDuplicate(String str,int i, StringBuilder newStr ,boolean map[]){
+        if(i == str.length()){
+            System.err.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(i);
+        if(map[currChar - 'a'] == true){
+            removeDuplicate(str,i+1,newStr,map);
+
+        }else{
+            if(map[currChar -'a']=true){
+                removeDuplicate(str,i+1,newStr.append(currChar),map);
+
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        String str = "appnnacollege";
+        removeDuplicate(str,0,new StringBuilder(" "),new boolean[26]);
+    }
+}
+
+public class Recursion{
+    public static int friendsPairing(int n){
+        if( n == 1 || n == 2){
+            return n;
+        }
+         int fnm1 = friendsPairing(n-1);
+         int fnm2 = (n-1)*friendsPairing(n-2);
+        return fnm1+fnm2;
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(friendsPairing(3));
+        
+    }
+    
+}
+
+
+public class Recursion{
+    public static void binaryString(int n,int lastPlace,StringBuilder str){
+        if(n == 0){
+            System.out.println(str);
+        }
+        if(lastPlace == 0){
+            binaryString(n-1,0,str.append("0"));
+            binaryString(n-1,1,str.append("1"));
+        }else{
+            binaryString(n-1,0,str.append("0"));
+
+        }
+
+    }
+    public static void main(String[] args) {
+        binaryString(3,0,new StringBuilder(" "));
+        
+    }
+    
+}
+
